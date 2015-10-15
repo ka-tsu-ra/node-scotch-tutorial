6481a2f01e9f2e0cb840673a5e74f5d984ef7427
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-10-15 12:34:12 +0100

    Adds routes in controller style for getting, creating and deleting todos

--- server.js
+++ server.js
@@ -18,8 +18,62 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 // define todo model
+
 var Todo = mongoose.model('Todo', {
   text : String
+});
+
+//routes setup
+
+// get all todos
+
+app.get('/api/todos', function(req, res) {
+
+ // use mongoose to get all todos in the database
+  Todo.find(function(err, todos) {
+
+ // if there's an error retrieving, send th eerror. nothign after res.send(err) will execute
+    if (err)
+      res.send(err)
+    res.json(todos); //return all todos in JSON format
+  });
+);
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+  
+  // create a todo - information comes from AJAX request from Angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+  //get and return all todos after creating a new one
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    // get and return all the todos after deleting this one
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
 });
 
 // listen (start app with node server.js) ======================================
